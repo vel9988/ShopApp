cc59b8a1ffb4e3151b3d00063ec87f3d4a2395b0
Dmitriy <58105647+vel9988@users.noreply.github.com>
2023-06-08 13:20:06 +0600

    Initial Commit

---
+++ ShopApp/ShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopAppApp.swift
+//  ShopApp
+//
+//  Created by Dmitryi Velko on 08.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
